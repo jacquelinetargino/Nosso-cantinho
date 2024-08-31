e7c7c3ec028d1fd36c37ccb7d6cf7830258af812
jacquelinetargino <jactargino146@gmail.com>
2024-08-31 20:36:09 -0300

    amo ele

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function revealMessage() {
+    const message = document.getElementById('specialMessage');
+    message.textContent = 'Eu te amo mais do que palavras podem expressar! Obrigado por ser incrível e fazer cada momento especial. 💕';
+}
